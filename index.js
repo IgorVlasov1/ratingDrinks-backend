e33ee875b743205257c1e7d54bd59d29ef364870
IgorVlasov1 <pwvoa17@gmail.com>
2023-09-11 15:29:26 +0300

    third

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const dotenv = require("dotenv");
-dotenv.config();
+// const dotenv = require("dotenv");
+// dotenv.config();
 const express = require("express");
 
 const sequelize = require("./config/db.config");
