5a9f537ea7babae70b0675ca5d79cc6b64204d29
IgorVlasov1 <pwvoa17@gmail.com>
2023-09-11 15:47:20 +0300

    five

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,21 @@
 const Sequelize = require("sequelize");
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 
-// dotenv.config();
+dotenv.config();
 
 const { DB_NAME, DB_PASSWORD, DB_USER, DB_URI, DB_HOST, DB_PORT } = process.env;
 
 // connect to db locally
-const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  dialect: "postgres",
-  host: DB_HOST,
-  port: DB_PORT,
+// const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//   dialect: "postgres",
+//   host: DB_HOST,
+//   port: DB_PORT,
+// });
+
+const db = new Sequelize(DB_URI, {
+  define: {
+    timestamps: false,
+  },
 });
+
 module.exports = db;
